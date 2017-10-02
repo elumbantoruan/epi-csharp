ba4eee2e102ff34340e9a7c0bf470b328da9f5b0
Edison Lumbantoruan <Edison.Lumbantoruan@gmail.com>
2017-10-02 14:35:45 -0700

    05-PrimitiveTypes

---
+++ 05-PrimitiveTypes/05.09-CheckIfADecimalIsPalindrome/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _05._09_CheckIfADecimalIsPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1221;
+            bool isPalindrome = IsPalindrome(n);
+            Debug.Assert(isPalindrome == true);
+
+            n = 1223;
+            isPalindrome = IsPalindrome(n);
+            Debug.Assert(isPalindrome == false);
+            
+        }
+
+        /*
+            Time complexity is O(n)
+            Space complexity is O(1)
+         */
+        static bool IsPalindrome(int n) {
+            while (n > 0) {
+                int numDigits = (int)Math.Log10(n) + 1;                
+                int mostSignificantDigit = n / (int)Math.Pow(10, numDigits - 1);
+                int leastSignificantDigit = n % 10;
+                if (mostSignificantDigit != leastSignificantDigit) {
+                    return false;
+                }
+                n -= mostSignificantDigit * (int)Math.Pow(10, numDigits - 1);   // remove most significant digit
+                n /= 10;                                                        // remove least signigicant digit
+            }
+            
+            return true;
+        }
+    }
+}
