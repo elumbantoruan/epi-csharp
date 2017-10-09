115fe85928b99e3353569431ca83facbe47233af
Edison Lumbantoruan <Edison.Lumbantoruan@gmail.com>
2017-10-09 11:03:37 -0700

    ch 8

--- 08-LinkedLists/08.01-MergeTwoSortedLinkedList/Program.cs
+++ 08-LinkedLists/08.01-MergeTwoSortedLinkedList/Program.cs
@@ -6,7 +6,51 @@ namespace _08._01_MergeTwoSortedLinkedList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ListNode l1 = new ListNode(2);
+            l1.Next = new ListNode(5);
+            l1.Next.Next = new ListNode(7);
+
+            ListNode l2 = new ListNode(3);
+            l2.Next = new ListNode(11);
+
+            ListNode mergedList = MergeSortedList(l1, l2);
+            ListNode current = mergedList;
+            while (current != null) {
+                Console.Write($"{current.Value} ");
+                current = current.Next;
+            }
+        }
+
+        static ListNode MergeSortedList(ListNode l1, ListNode l2) {
+            ListNode mergedList = new ListNode(-1);
+            ListNode iterator = mergedList;
+
+            while (l1 != null && l2 !=  null) {
+                if (l1.Value < l2.Value) {
+                    iterator.Next = new ListNode(l1.Value);
+                    iterator = iterator.Next;
+                    l1 = l1.Next;
+                }
+                else {
+                    iterator.Next = new ListNode(l2.Value);
+                    iterator = iterator.Next;
+                    l2 = l2.Next;
+                }
+            }
+
+            iterator.Next =  (l1 == null ? l2 : l1);
+
+            return mergedList.Next;
+        }
+    }
+
+    public class ListNode {
+        public int Value { get; set; }
+        public ListNode Next { get; set; }
+
+        public ListNode(int value)
+        {
+            Value = value;
         }
     }
 }
---
+++ 08-LinkedLists/08.04-TestForOverlapping/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _08._04_TestForOverlapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode l1 = new ListNode(4);
+            l1.Next = new ListNode(5);
+            l1.Next.Next = new ListNode(6);
+            l1.Next.Next.Next = new ListNode(7);
+            l1.Next.Next.Next.Next = new ListNode(8);
+
+            ListNode l2 = new ListNode(1);
+            l2.Next = l1.Next.Next;
+
+            ListNode overlapping = FindOverlapping(l1, l2);
+            Console.WriteLine($"{(overlapping == null ? "none" : overlapping.Value.ToString())}");
+        }
+
+        static ListNode FindOverlapping(ListNode node1, ListNode node2) {
+            int lengthNode1 = Length(node1);
+            int lengthNode2 = Length(node2);
+            if (lengthNode1 > lengthNode2) {
+                node1 = AdvanceTheNode(node1, lengthNode1 - lengthNode2);
+            }
+            else {
+                node2 = AdvanceTheNode(node2, lengthNode2 - lengthNode1);
+            }
+            while (node1 != null && node2 != null) {
+                if (node1.Value == node2.Value) {
+                    return node1;
+                }
+                node1 = node1.Next;
+                node2 = node2.Next;
+            }
+            return null;
+        }
+
+        static int Length(ListNode node) {
+            int length = 0;
+            ListNode current = node;
+            while (current != null) {
+                current = current.Next;
+                length++;
+            }
+            return length;
+        }
+
+        static ListNode AdvanceTheNode(ListNode node, int n) {
+            while (n > 0) {
+                node = node.Next;
+                n--;
+            }
+            return node;
+        }
+    }
+
+    public class ListNode {
+        public int Value { get; set; }
+        public ListNode Next { get; set; }
+        public ListNode(int value)
+        {
+            Value = value;
+        }
+    }
+}
