1a67bb89fcc17c7574c783f74e3c2684dd710eaf
Edison Lumbantoruan <Edison.Lumbantoruan@gmail.com>
2017-10-20 12:21:55 -0700

    fix bug 06.03-MultiplyNumbers and LICENSE

--- 06-Arrays/06.03-MultiplyNumbers/Program.cs
+++ 06-Arrays/06.03-MultiplyNumbers/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace _06._03_MultiplyNumbers
 {
@@ -14,6 +15,11 @@ namespace _06._03_MultiplyNumbers
             {
                 Console.Write($"{item} ");
             }
+            List<int> expected = new List<int> {7,5};
+            for (int i = 0; i < results.Count; i++)
+            {
+                Debug.Assert(results[i] == expected[i]);
+            }
             Console.WriteLine();
             n1 = new List<int> { -2, 5};
             n2 = new List<int> { 3 };
@@ -21,6 +27,24 @@ namespace _06._03_MultiplyNumbers
             foreach (var item in results)
             {
                 Console.Write($"{item} ");
+            }
+            expected = new List<int> {-7,5};
+            for (int i = 0; i < results.Count; i++)
+            {
+                Debug.Assert(results[i] == expected[i]);
+            }
+            Console.WriteLine();
+            n1 = new List<int> { -2, 5};
+            n2 = new List<int> { -2, 5 };
+            results = Multiply(n1, n2);
+            foreach (var item in results)
+            {
+                Console.Write($"{item} ");
+            }
+            expected = new List<int> {6,2,5};
+            for (int i = 0; i < results.Count; i++)
+            {
+                Debug.Assert(results[i] == expected[i]);
             }
         }
 
@@ -33,19 +57,28 @@ namespace _06._03_MultiplyNumbers
             if (n1[0] * n2[0] < 0) {
                 signed = -1;
             }
-            List<int> results = new List<int>();
+            int[] arr = new int[n1.Count + n2.Count];
+            List<int> results = new List<int>(arr);
             int result = 0;
             int carryOver = 0;
             for (int i = n1.Count - 1; i >= 0; i--) {
                 for (int j = n2.Count - 1; j >=0; j--) {
                     result = Math.Abs(n1[i] * n2[j]) + carryOver;
-                    results.Insert(0, result % 10);
+                    results[i + j + 1] += result % 10;
                     carryOver = result / 10;
+
+                    if (carryOver > 0) {
+                        results[i + j] += carryOver;
+                        carryOver = 0;
+                    }
                 }
             }
-            if (carryOver > 0) {
-                results.Insert(0, carryOver);
+
+            if (results[0] == 0)
+            {
+                results.RemoveAt(0);
             }
+
             results[0] *= signed;
             return results;
         }
