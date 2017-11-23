c45874907c07fcacf10d36fa5ace7992913abb85
Edison Lumbantoruan <Edison.Lumbantoruan@gmail.com>
2017-11-22 22:38:15 -0800

    add DutchNationalFlag without pivot

--- 06-Arrays/06.01-TheDutchNationalFlagProblem/Program.cs
+++ 06-Arrays/06.01-TheDutchNationalFlagProblem/Program.cs
@@ -16,12 +16,28 @@ namespace _06._01_TheDutchNationalFlagProblem
                 Color.Red,
                 Color.White
             };
-            DutchFlagPartition3(1, colors);
+            DutchFlagPartitionWithPivot(1, colors);
             foreach (var item in colors) {
                 Console.WriteLine($"{item}");
             }
-            
+
+            Console.WriteLine();
+
+            colors = new List<Color> {
+                Color.Blue,
+                Color.Red,
+                Color.Red,
+                Color.Blue,
+                Color.White,
+                Color.Red,
+                Color.White
+            };
+            DutchFlagPartitionWithoutPivot(colors);
+            foreach (var item in colors) {
+                Console.WriteLine($"{item}");
+            }
         }
+
 
         /*
             Given unsorted array, sort the array based on the pivot index
@@ -33,59 +49,14 @@ namespace _06._01_TheDutchNationalFlagProblem
 
             pivot index 2  A[2] = 2
             result: {0,1,0,1,1,2,2} or {0,0,1,1,1,2,2}
-         */
-        static void DutchFlagPartition(int pivotIndex, List<Color> list) {
-            Color pivot = list[pivotIndex];
-            // first pass, group elements smaller than pivot
-            int smaller = 0;
-            for (int i = 0; i < list.Count; i++) {
-                if ((int)list[i] < (int)pivot) {
-                    Swap(list, smaller++, i);
-                }
-            }
-
-            // second pass, group elements larger than pivot
-            int larger = list.Count - 1;
-            for (int i = list.Count - 1; i >= 0; i--) {
-                if ((int)list[i] >= (int)pivot) {
-                    if ((int)list[i] > (int)pivot) {
-                        Swap(list, larger--, i);
-                    }
-                }
-            }
-        }
-
-        static void DutchFlagPartition2(int pivotIndex, List<Color> list) {
-            Color pivot = list[pivotIndex];
-
-            /*
-                Keep the following invariants during partitioning
-                bottom goup; A sublist(0, smaller)
-                middle group: A sublist (smaller, equal)
-                unclassified: A sublist (equal, larger)
-                top: A sublist(larger, A.Count)
-             */
-             int smaller = 0;
-             int equal = 0;
-             int larger = list.Count;
-
-             while (equal < larger) {
-                 if ((int)list[equal] < (int)pivot) {
-                     Swap(list, smaller, equal);
-                     smaller++;
-                     equal++;
-                 }
-                 else if ((int)list[equal] == (int)pivot) {
-                     equal++;
-                 }
-                 else {
-                     Swap(list, equal, larger);
-                     larger--;
-                 }
-             }
-        }
-
-        static void DutchFlagPartition3(int pivotIndex, List<Color> list) {
+    
+            Keep the following invariants during partitioning
+            bottom goup; A sublist(0, smaller)
+            middle group: A sublist (smaller, equal)
+            unclassified: A sublist (equal, larger)
+            top: A sublist(larger, A.Count)
+        */
+        static void DutchFlagPartitionWithPivot(int pivotIndex, List<Color> list) {
             int i = 0;
             int j = 0;
             int n = list.Count - 1;
@@ -98,12 +69,33 @@ namespace _06._01_TheDutchNationalFlagProblem
                     i++;
                     j++;
                 }
+                else if ((int)list[j] == (int)pivot) {
+                    j++;
+                }
                 else if ((int)list[j] > (int)pivot) {
                     Swap(list, j, n);
                     n--;
                 }
-                else {
+            }
+        }
+
+        static void DutchFlagPartitionWithoutPivot(List<Color> colors) {
+            int i = 0;
+            int j = 0;
+            int n = colors.Count - 1;
+
+            while (j < n) {
+                if (colors[j] == Color.Red) {
+                    Swap(colors, i, j);
+                    i++;
                     j++;
+                }
+                else if (colors[j] == Color.White) {
+                    j++;
+                }
+                else if (colors[j] == Color.Blue) {
+                    Swap(colors, j, n);
+                    n--;
                 }
             }
         }
